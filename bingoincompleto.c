69381128409f74b53adfcefb9dce2de9c907ac8a
Joao Alonso <alonso963@hotmail.com>
2020-04-16 11:54:23 -0300

    desisti de deixar bonitin, vou mandar tudo msm

--- bingoincompleto.c
+++ bingoincompleto.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
 
-    int tab[5][5], lin, col, i;
+
+int main()
+{
+
+    int tab[5][5], lin, col;
 
     srand (time(NULL));
 
 
-    for (col = 0; col < 5; col++) {
-        for (lin = 0; lin < 5; lin++){
+    for (lin = 0; lin < 5; lin++)
+    {
+        for (col = 0; col < 5; col++)
+        {
             tab[lin][col] = rand() % 100;
-            printf ("%d ", tab[lin][col] );
+
+
+            for (int i = 4; i >= 0 && i != lin; i--)
+            {
+                for (int j = 4; j >= 0 && j!= col; j--)
+                {
+                    if (tab[lin][col] == tab[i][j])
+                    //vai comparar tab[lin][col] com a tabela toda menos com ele mesmo, até com os valores que ainda nao foram definidos
+                    {
+
+                        if (tab[lin][col] == 0)
+                        //para nao ter valores negativos
+                        {
+                            tab[lin][col] ++;
+                        }
+                        else if (tab[lin][col] == 99)
+                        //para nao ter numeros > 99
+                        {
+                            tab[lin][col] --;
+                        }
+                        else
+                        {
+                            tab[lin][col] ++;
+                        }
+                    }
+                }
+            }
+            if (tab[lin][col] < 10)
+            //para que a formatacao fique coerente
+            {
+                printf ("%d   ", tab[lin][col]);
+            }
+            else
+            {
+                printf ("%d  ", tab[lin][col]);
+            }
         }
-        printf ("\n");
+        printf("\n");
     }
 }
---
+++ ex2ponteiros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void separa(float *x);
+
+int main (){
+    float num;
+    printf ("digite o numero real: ");
+    scanf ("%f", &num);
+    separa (&num);
+}
+
+void separa(float *x){
+int *inteiro, *fracao;
+}
